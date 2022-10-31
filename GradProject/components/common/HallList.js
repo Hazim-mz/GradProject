46c83d190a1c580988462f3153d6501ff3dcbe3a
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2022-11-01 02:20:06 +0300

    Update HallList.js

--- GradProject/components/common/HallList.js
+++ GradProject/components/common/HallList.js
@@ -7,11 +7,7 @@ const wait = (timeout) => {
     return new Promise(resolve => setTimeout(resolve, timeout));
 }
 
-
 function HallList({Halls, navigation}){
-
-function HallList({Halls}){
-
     const [refreshing, setRefreshing] = useState(false);
     const onRefresh = useCallback(() => {
         setRefreshing(true);
@@ -23,7 +19,6 @@ function HallList({Halls}){
         const item = itemData.item;
         const hallInfoProps ={
             id: item.id, //هذا عشان نعرف ايدي قاعة اللي بننتقل لها بعدين
-            id: item.id, //هذا عشان نعرف ايدي الاكله اللي بننتقل لها بعدين
             name: item.name,
             price: item.price,
             guests: item.guests,
@@ -46,8 +41,6 @@ function HallList({Halls}){
                     <RefreshControl
                         refreshing={refreshing}
                         onRefresh={onRefresh}
-                      refreshing={refreshing}
-                      onRefresh={onRefresh}
                     />
                 }
             />
@@ -64,4 +57,4 @@ const styles = StyleSheet.create({
     flat1:{
         flex: 1,
     },
-});
+  });
