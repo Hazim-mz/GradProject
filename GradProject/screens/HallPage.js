b1b6daa16e49f20e662c1da044b28938e60eaa86
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2022-12-17 22:14:22 +0300

    Update HallPage.js

--- GradProject/screens/HallPage.js
+++ GradProject/screens/HallPage.js
@@ -1,15 +1,22 @@
 import { useLayoutEffect } from "react";
-import { View, Text, Pressable, ScrollView,StyleSheet } from "react-native";
+import { View, Text, Pressable, ScrollView, TextInput, KeyboardAvoidingView, Button,StyleSheet } from "react-native";
 
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 import { FontAwesome5 } from '@expo/vector-icons'; 
 
 import { HALLS } from '../data/dummy-data';
+import { COMMENTS } from '../data/dummy-data2';
 import HallImage from "../components/common/HallImage";
 import { GlobalStyles } from "../constants/styles";
 import MainInformation from "../components/hallCom/MainInformation";
 import RoomInformation from "../components/hallCom/RoomInformation";
+import ServiecesIcon from "../components/hallCom/ServiecesIcon";
+import DescriptionInformation from "../components/hallCom/DescriptionInformation";
+import ServiecesInformation from "../components/hallCom/ServiecesInformation";
+import HallForm from "../components/ManageHall/HallForm";
+import EnterReview from "../components/hallCom/EnterReview";
+import HallComment from "../components/hallCom/HallComment";
 
 
 function HallPage({route, navigation}){
@@ -32,6 +39,9 @@ function HallPage({route, navigation}){
     const displayedHall = HALLS.filter((hallItem) => {
         return hallItem.id == HallId;
     });
+    const displayedComment = COMMENTS.filter((commentItem) => {
+        return commentItem.hallID == HallId;
+    });
 
     function pressLoctionHandler(){
         return;
@@ -39,8 +49,7 @@ function HallPage({route, navigation}){
 
     return(
         <View style={styles.container}>
-            <ScrollView>
-
+            <ScrollView style={{flex: 1}}>
                 <View style={{flex: 1}}>
                     <HallImage 
                         data={displayedHall[0].imageUrl}
@@ -48,16 +57,42 @@ function HallPage({route, navigation}){
                     />
                 </View>
 
-                <View style={styles.InfoContainer}>
+                <ScrollView style={{flex: 1}}>
+                    <KeyboardAvoidingView style={{flex:1}} behavior="padding">
+                        <View style={styles.InfoContainer}>
 
-                    <MainInformation data={displayedHall} onPress={pressLoctionHandler}/>
+                            {/* المعلومات الاساسية */}
+                            <MainInformation data={displayedHall} onPress={pressLoctionHandler}/>
 
-                    <View style={styles.line}></View>
+                            <View style={styles.line}></View>
 
-                    <RoomInformation />
+                            {/* المعلومات الغرف */}
+                            <RoomInformation />
 
-                    <View style={styles.line}></View>
-                </View>
+                            <View style={styles.line}></View>
+
+                            <View style={styles.descServContainer}>
+
+                                {/* تعريف القاعة  */}
+                                <DescriptionInformation description={displayedHall[0].description}/>
+
+                                {/* الخدمات */}
+                                <ServiecesInformation services={displayedHall[0].services}/>
+                                
+                            </View>
+
+                            <View style={styles.line}></View>
+
+                            <EnterReview />
+
+                            <View style={styles.line2}></View>
+
+                            <HallComment data={displayedComment}/>
+                            
+
+                        </View>
+                    </KeyboardAvoidingView>
+                </ScrollView>
             </ScrollView>
         </View>
     );
@@ -85,6 +120,12 @@ const styles = StyleSheet.create({
         margintop: 4,
         marginBottom: 6,
     },
+    line2:{
+        backgroundColor:'white',
+        borderBottomWidth: 1,
+        marginRight: 60,
+        marginLeft: 4,
+    },
     boldName:{
         fontSize: 16,
         fontWeight: 'bold',
@@ -98,7 +139,43 @@ const styles = StyleSheet.create({
         fontSize: 12,
         textAlign:'center',
     },
-    button:{
-        opacity: 0.8,
+    descServContainer:{
+        flex: 1,
+        backgroundColor: 'white',
+        borderRadius:3,
+        marginHorizontal: 4,
+        paddingTop:8,
+        paddingLeft:6,
+    },
+    commentContainer:{
+        flex: 1,
+        backgroundColor: 'white',
+        borderRadius:3,
+        marginHorizontal: 4,
+        paddingTop:8,
+        paddingLeft:6,
+        minHeight: 180,
+    },
+    textIput1:{
+        color: GlobalStyles.colors.primary700,
+        borderWidth: 2,
+        borderColor: '#dfa4f5',
+        marginTop: 4,
+        padding: 6,
+        borderRadius: 6,
+        fontSize: 18,
+        minHeight: 100,
+        maxHeight: 150,
+        textAlignVertical: 'top',
+    },
+    ratingContainer:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    ratingContainer2:{
+        flex: 1,
+        flexDirection: 'row',
+        marginTop: 4,
     },
 });
