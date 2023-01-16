4abfc2b54587990d0ba3684a2838208b3e4c5b09
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2023-01-16 08:58:14 +0300

    Update Date.js

--- GradProject/components/hallCom/Date.js
+++ GradProject/components/hallCom/Date.js
@@ -3,13 +3,14 @@ import { useState, useEffect } from "react";
 
 import { Calendar } from "react-native-calendars";
 
-function Date({visible, dateFromCalendar, bookedDates, length}){
-    
+
+function Date({visible, dateFromCalendar, bookedDates, todayDate}){
+
     //Make booked days inactive
     const disabled = { disabled: true, disableTouchEvent: true };
     let markedDates = {};
-    for(var i=0; i<length; i++){
-        markedDates[bookedDates[i]] = disabled;
+    for(var i=0; i<bookedDates.length; i++){
+        markedDates[bookedDates[i].data.Date] = disabled;
         //console.log(markedDates);
     }
 
@@ -18,9 +19,11 @@ function Date({visible, dateFromCalendar, bookedDates, length}){
             <View style={styles.container}>
                 <Calendar 
                     onDayPress={date => {
-                        dateFromCalendar.bind(this, date.dateString)();
+                        dateFromCalendar.bind(this, date.dateString, date.day)();
                     }}
                     markedDates={markedDates}
+                    minDate={todayDate}
+                    
                 />
             </View>
         </Modal>
