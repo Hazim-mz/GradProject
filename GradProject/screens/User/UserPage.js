8a81e8b0e045057f6407d07e8cf25f56ed792fc7
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2023-01-20 10:31:08 +0300

    Update UserPage.js

--- GradProject/screens/User/UserPage.js
+++ GradProject/screens/User/UserPage.js
@@ -10,14 +10,41 @@ import { Ionicons } from '@expo/vector-icons';
 import { Foundation } from '@expo/vector-icons'; 
 
 import { GlobalStyles } from '../../constants/styles';
+
 import Home from './Home';
 import HallPage from './HallPage';
 import Inbox from './Inbox';
 import Bookings from './Bookings';
 import Account from './Account';
 
+import LoginScreen from '../LoginScreen';
+import SignupScreen from '../SignupScreen';
+import AuthContextProvider from '../../store/auth-context';
+
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
+
+function LoginOverview() {
+    return (
+        <Stack.Navigator
+        screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary10 },
+            headerTintColor: 'white',
+            headerShown: false,
+        }}
+        >
+        <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+        />
+        <Stack.Screen
+            name="Signup"
+            component={SignupScreen}
+        />
+
+        </Stack.Navigator>
+    );
+}
 
 function HomeOverview() {
     return (
@@ -44,64 +71,78 @@ function UserPage() {
     
     return (
         <>
-        <StatusBar style="auto" />
-
-        <NavigationContainer>
-            <BottomTabs.Navigator
-                screenOptions={() => ({
-                    headerStyle: { backgroundColor: GlobalStyles.colors.primary10 },
-                    headerTintColor: 'white',
-                    //tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-                    tabBarActiveTintColor: GlobalStyles.colors.primary10,
-                })}
-            >
-            <BottomTabs.Screen
-                name="HomeOverview"
-                component={HomeOverview}
-                options={{
-                headerShown: false,
-                title: 'Home',
-                tabBarLabel: 'Home',
-                tabBarIcon: ({ color, size }) => (
-                    <Ionicons name="home" size={size} color={color} />
-                ),
-                }}
-            />
-            <BottomTabs.Screen
-                name="Inbox"
-                component={Inbox}
-                options={{
-                title: 'Inbox',
-                tabBarLabel: 'Inbox',
-                tabBarIcon: ({ color, size }) => (
-                    <Ionicons name="mail" size={size} color={color} />
-                ),
-                }}
-            />
-            <BottomTabs.Screen
-                name="Bookings"
-                component={Bookings}
-                options={{
-                title: 'Bookings',
-                tabBarLabel: 'Bookings',
-                tabBarIcon: ({ color, size }) => (
-                    <Foundation name="shopping-bag" size={size} color={color} />
-                ),
-                }}
-            />
-            <BottomTabs.Screen
-                name="Account"
-                component={Account}
-                options={{
-                title: 'Account',
-                tabBarLabel: 'Account',
-                tabBarIcon: ({ color, size }) => (
-                    <Ionicons name="person" size={size} color={color} />
-                ),
-                }}
-            />
-            </BottomTabs.Navigator>
-        </NavigationContainer>
+            <NavigationContainer>
+                <BottomTabs.Navigator
+                    screenOptions={() => ({
+                        headerStyle: { backgroundColor: GlobalStyles.colors.primary10 },
+                        headerTintColor: 'white',
+                        //tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+                        tabBarActiveTintColor: GlobalStyles.colors.primary10,
+                    })}
+                >
+                <BottomTabs.Screen
+                    name="HomeOverview"
+                    component={HomeOverview}
+                    options={{
+                        headerShown: false,
+                        title: 'Home',
+                        tabBarLabel: 'Home',
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="home" size={size} color={color} />
+                        ),
+                    }}
+                />
+                <BottomTabs.Screen
+                    name="Inbox"
+                    component={Inbox}
+                    options={{
+                        title: 'Inbox',
+                        tabBarLabel: 'Inbox',
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="mail" size={size} color={color} />
+                        ),
+                    }}
+                />
+                <BottomTabs.Screen
+                    name="Bookings"
+                    component={Bookings}
+                    options={{
+                        title: 'Bookings',
+                        tabBarLabel: 'Bookings',
+                        tabBarIcon: ({ color, size }) => (
+                            <Foundation name="shopping-bag" size={size} color={color} />
+                        ),
+                    }}
+                />
+                <BottomTabs.Screen
+                    name="Account"
+                    component={Account}
+                    options={{
+                        title: 'Account',
+                        tabBarLabel: 'Account',
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="person" size={size} color={color} />
+                        ),
+                    }}
+                />
+                <BottomTabs.Screen
+                    name="LoginOverview"
+                    component={LoginOverview}
+                    options={{
+                        tabBarStyle: {
+                            display: "none",
+                        },
+                        tabBarButton: () => null,
+                        headerShown: false,
+                        title: 'Login',
+                        tabBarLabel: 'Login',
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="home" size={size} color={color} />
+                        ),
+                    }}
+                />
+                </BottomTabs.Navigator>
+            </NavigationContainer>
         </>
     );
 }
