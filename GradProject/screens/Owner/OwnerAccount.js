6804c61e84ab529fc9d97ee6ef311a6ebecee774
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2023-01-20 11:08:43 +0300

    Add files via upload

---
+++ GradProject/screens/Owner/OwnerAccount.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function OwnerAccount(){
+    return(
+        <View>
+            <Text>Account page</Text>
+        </View>
+    );
+}
+
+export default OwnerAccount;
