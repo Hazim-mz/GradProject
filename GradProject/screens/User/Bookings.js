1ae5a3aa37722ce7253143ed1f517f0fe3f6da22
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2023-01-20 10:30:10 +0300

    Update Bookings.js

--- GradProject/screens/User/Bookings.js
+++ GradProject/screens/User/Bookings.js
@@ -1,12 +1,41 @@
-import { View, Text } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { useContext, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import { AuthContext } from "../../store/auth-context";
 
 
-function Bookings(){
-    return(
-        <View>
-            <Text>Bookings page</Text>
-        </View>
-    );
+function Bookings({navigation}){
+    const userAccountCtx = useContext(AuthContext);
+
+    function onPress(){
+        navigation.navigate("LoginOverview");
+    }
+    //console.log(userAccountCtx);
+    if(! userAccountCtx.isAuthenticated){
+        return(
+            <View style={styles.container}>
+                <Text>Login First To See Your Booked Halls</Text>
+                <Button title="Login" onPress={onPress}/>
+            </View>
+        );
+    }
+    else{
+        return(
+            <View style={styles.container}>
+                <Text>Welcome {userAccountCtx.email}</Text>
+                <Button title="Login" onPress={onPress}/>
+            </View>
+        );
+    }
 }
 
 export default Bookings;
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        marginTop: 10
+    },
+});
+  
