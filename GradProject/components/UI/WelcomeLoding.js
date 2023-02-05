10cf36980c6d7384058eb35b33c98978dacbfdbd
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2023-02-06 00:54:32 +0300

    Update WelcomeLoding.js

--- GradProject/components/UI/WelcomeLoding.js
+++ GradProject/components/UI/WelcomeLoding.js
@@ -1,44 +1,27 @@
-import { View, StyleSheet } from "react-native";
-import { useEffect, useRef, useCallback } from 'react'
+import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
 
-import LottieView from 'lottie-react-native';
-import AnimatedLottieView from 'lottie-react-native';
-import { GlobalStyles } from "../../constants/styles";
-
-
-function WelcomeLoding({source, style, onAnimationFinish, autoplay = true, loop = true, speed = 1.5}){
-
-  const lottieRef = useRef(null);
-  useEffect(() => {
-      lottieRef.current?.reset();
-      setTimeout(() => {
-          lottieRef.current?.play();
-      }, 0)
-
-  }, []);  
-
-  return(
-      <View style={[styles.view1, StyleSheet.absoluteFillObject]}>
-          <AnimatedLottieView 
-              ref={lottieRef}
-              source={require('../../assets/lf30_editor_zq9c5wtz.json')} 
-              style={{ flex: 1,height: 350}}
-              autoPlay={autoplay}
-              loop={loop}
-              speed={speed}
-          />
-      </View>
-  );
+function LodingOverlay({text}){
+    return(
+        <View style={styles.view1}>
+            <Text style={styles.text1}>{text}</Text>
+            <ActivityIndicator size="large" color="white"/>
+        </View>
+    );
 }
 
-export default WelcomeLoding;
+export default LodingOverlay;
 
 const styles = StyleSheet.create({
     view1:{
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: GlobalStyles.colors.primary10,
-        zIndex: 1,
+        padding: 24,
+        backgroundColor: '#E8E8E8',
+    },
+    text1:{
+        marginBottom:  4,
+        fontSize: 16,
+        fontWeight: "bold",
     },
 });
