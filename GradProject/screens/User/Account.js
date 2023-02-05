c4fe0824bf9b0fc3b3845a070589140af528ae41
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2023-02-06 00:46:45 +0300

    Update Account.js

--- GradProject/screens/User/Account.js
+++ GradProject/screens/User/Account.js
@@ -1,109 +1,114 @@
-import { View, Text, Button, Image, StyleSheet, Pressable } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 import { useContext, useState, useEffect } from 'react';
-import { Entypo } from '@expo/vector-icons'; 
+import { useNavigation } from '@react-navigation/native';
 
 import { AuthContext } from "../../store/auth-context";
-import ChangeImage from "../../components/accountCom/ChangeImage";
 
 import { db } from '../../config'; 
-import { collection, where, query, onSnapshot, getDocs} from 'firebase/firestore';
-import ChangeName from "../../components/accountCom/ChangeName";
-import { GlobalStyles } from "../../constants/styles";
-import ChangeEmail from "../../components/accountCom/ChangeEmail";
-import ChangePassword from "../../components/accountCom/ChangePassword";
-import ChangeHallID from "../../components/accountCom/ChangeHallID";
+import { collection, where, query, onSnapshot, getDocs, deleteDoc } from 'firebase/firestore';
 
-function Account({navigation, route}){
+import HallList from "../../components/common/HallList";
+
+function Bookings({navigation, route}){
     const userAccountCtx = useContext(AuthContext);
-    //console.log(userAccountCtx);
+    //console.log(userAccountCtx); 
     function onPress(){
         navigation.navigate("LoginOverview");
     }
 
-    //change Image
-    const [imageIsVisible, setImageIsVisible] = useState(false);
-    function openImage(){
-        setImageIsVisible(true);
-    }
-    function closeImage(){
-        setImageIsVisible(false);
-    }
-    function changeImage(image){
-        setImage(image);
-        userAccountCtx.image = image;
-    }
-    //change Name
-    const [nameIsVisible, setNameIsVisible] = useState(false);
-    function openName(){
-        setNameIsVisible(true);
-    }
-    function closeName(){
-        setNameIsVisible(false);
-    }
-    function changeName(Name){
-        userAccountCtx.name = Name;
-    }
+    const [Halls, setHalls] = useState([]);//all halls
+    const [reservations, setReservations] = useState([]);//reservations of user
+    const [bookedHalls, setBookedHalls] = useState([]);//halls reserved by the user
 
-    //change Email
-    const [emailIsVisible, setEmailIsVisible] = useState(false);
-    function openEmail(){
-        setEmailIsVisible(true);
-    }
-    function closeEmail(){
-        setEmailIsVisible(false);
-    }
-    function changeEmail(idToken, email){
-        userAccountCtx.token = idToken;
-        userAccountCtx.email = email;
-    }
-
-    //change Pasword
-    const [passwordIsVisible, setPaswordIsVisible] = useState(false);
-    function openPasword(){
-        setPaswordIsVisible(true);
-    }
-    function closePasword(){
-        setPaswordIsVisible(false);
-    }
-    function changePasword(idToken){
-        userAccountCtx.token = idToken;
-    }
-
-    //change Hall ID if he owner
-    const [hallIDIsVisible, setHallIDIsVisible] = useState(false);
-    function openHallID(){
-        setHallIDIsVisible(true);
-    }
-    function closeHallID(){
-        setHallIDIsVisible(false);
-    }
-    function changeHallID(record){
-        userAccountCtx.record = record;
-    }
-
-    const [numberOfReservations, setNumberOfReservations] = useState([]);
-    const [image, setImage] = useState('https://icons.veryicon.com/png/o/internet--web/55-common-web-icons/person-4.png');
+    //get reservations of user
     useEffect(()=>{
-        if(userAccountCtx.isAuthenticated && userAccountCtx.userID !== undefined){
+        const ref = collection(db, "Halls");
+        onSnapshot(ref, (Halls) =>
+            setHalls(Halls.docs.map((Hall) =>({
+                    id: Hall.id,
+                    data: Hall.data()
+                }))
+            )
+        );
+        if(userAccountCtx.isAuthenticated && userAccountCtx.userID !== undefined ){
             const dates = query(collection(db, "Reservation"), where("UserID", "==", userAccountCtx.userID));
             onSnapshot(dates, (Reservation) =>{
-                setNumberOfReservations(Reservation.docs.map((reservations) =>({
+                setReservations(Reservation.docs.map((reservations) =>({
                         id: reservations.id,
                         data: reservations.data()
                     }))
                 )
             });
-            setImage(userAccountCtx.image);
         }
-    },[userAccountCtx.isAuthenticated]);
+    },[userAccountCtx.isAuthenticated, route.params?.update]);
 
+    //after get reservations of user get the halls the user reserved 
+    function requiredMediaByName (id){
+        return Halls.find(item => item.id === id);
+    };
+    //to check if the user can cancel the reservation or not  
+    var day = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+    var todayDate ;
+    if(day < 10 && month < 10)
+        todayDate = year + '-0' + month + '-0'+day;
+    else if(day < 10)
+        todayDate = year + '-' + month + '-0'+day;
+    else if(month < 10)
+        todayDate = year + '-0' + month + '-'+day;
+    else
+        todayDate = year + '-' + month + '-'+day;
+    
+    function isLater(dateString1) {
+        return dateString1 > todayDate
+    }
 
+    useEffect(()=>{
+        if(reservations.length>0){
+            //let HallsID = reservations.map(item => item.data.HallsID);
+            //console.log(HallsID);
+            //let BookedHalls = Halls.filter(item => (HallsID.includes(item.id)));
+            //console.log(BookedHalls);
 
+            //sort hall by the date to make the new in top 
+            list= reservations.sort((a, b) => (a.data.Date < b.data.Date) ? 1 : -1)
+
+            let BookedHalls = [];
+            let temp = {};
+            for(var i=0; i<list.length ; i++){
+                temp = requiredMediaByName(list[i].data.HallsID);
+                if(isLater(list[i].data.Date))
+                    temp = {...temp, cancelAvailable: true, Date: list[i].data.Date}
+                else
+                    temp = {...temp, cancelAvailable: false, Date: list[i].data.Date}
+                BookedHalls.push(temp);
+            }
+
+            setBookedHalls(BookedHalls); 
+
+        }
+        else{
+            setBookedHalls([]);
+        }
+    },[reservations]);
+
+    //console.log('Halls: ', Halls); 
+    //console.log('reservations of user: ', reservations);
+    //console.log('halls reserved by the user: ', bookedHalls);
+
+    async function deleateReservations(hallID, Date){
+        const q = query(collection(db, "Reservation"), where("UserID", "==", userAccountCtx.userID), where("HallsID", "==", hallID), where("Date", "==", Date));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            deleteDoc(doc.ref);
+        });
+    }
 
     if(! userAccountCtx.isAuthenticated){
         return(
             <View style={styles.container}>
-                <Text>Login First To See Your Account information</Text>
+                <Text>Login First To See Your Booked Halls</Text>
                 <Button title="Login" onPress={onPress}/>
             </View>
         );
@@ -111,238 +116,27 @@ function Account({navigation, route}){
     else{
         return(
             <View style={styles.container}>
-
-                <ChangeImage userID={userAccountCtx.userID} image={image} changeImage={changeImage} visible={imageIsVisible} close={closeImage}/>
-
-                <ChangeName oldName={userAccountCtx.name} userID={userAccountCtx.userID} changeName={changeName} visible={nameIsVisible} close={closeName}/>
-
-                <ChangeEmail oldEmail={userAccountCtx.email} userID={userAccountCtx.userID} idToken={userAccountCtx.token} changeEmail={changeEmail} visible={emailIsVisible} close={closeEmail}/>
-
-                <ChangePassword userID={userAccountCtx.userID} idToken={userAccountCtx.token} changePasword={changePasword} visible={passwordIsVisible} close={closePasword}/>
-
-                <ChangeHallID oldHallID={userAccountCtx.record} userID={userAccountCtx.userID} changeHallID={changeHallID} visible={hallIDIsVisible} close={closeHallID}/>
-
-                <View style={styles.subContainer1}>
-                    <View style={styles.imageContainer}>
-                            <Pressable
-                                style={({pressed}) => (pressed ? styles.buttonPress : null)}
-                                onPress={openImage}
-                            >
-                                <Image                                     
-                                    source={{uri: image}}
-                                    style={styles.image}
-                                />
-                            </Pressable>
-                    </View>
-                </View>
-                <View style={styles.subContainer2}>
-                    <View style={styles.informationContainer}>
-
-                        <View style={styles.nameContainer}>
-                            <View style={styles.name}>
-                                <Text style={styles.nameLabel}>Name: </Text>
-                                <Text style={styles.nameText}>{userAccountCtx.name}</Text>
-                            </View>
-                            <Pressable
-                                style={({pressed}) => (pressed ? [styles.buttonPress, styles.button] : styles.button)}
-                                onPress={openName}
-                            >
-                                <Entypo name="edit" size={18} color="black" />
-                            </Pressable>
-                        </View>
-
-                        <View style={styles.emailContainer}>
-                            <View style={styles.email}>
-                                <Text style={styles.emailLabel}>Email: </Text>
-                                <Text style={styles.emailText}>{userAccountCtx.email}</Text>
-                            </View>
-                            <Pressable
-                                style={({pressed}) => (pressed ? [styles.buttonPress, styles.button] : styles.button)}
-                                onPress={openEmail}
-                            >
-                                <Entypo name="edit" size={18} color="black" />
-                            </Pressable>
-                        </View>
-
-                        <View style={styles.emailContainer}>
-                            <View style={styles.email}>
-                                <Text style={styles.emailLabel}>Password: </Text>
-                                <Text style={styles.emailText}>*********</Text>
-                            </View>
-                            <Pressable
-                                style={({pressed}) => (pressed ? [styles.buttonPress, styles.button] : styles.button)}
-                                onPress={openPasword}
-                            >
-                                <Entypo name="edit" size={18} color="black" />
-                            </Pressable>
-                        </View>
-
-                        {
-                            userAccountCtx.rule > 0 ?
-                                <View style={styles.emailContainer}>
-                                    <View style={styles.email}>
-                                        <Text style={styles.emailLabel}>Hall ID: </Text>
-                                        <Text style={styles.emailText}>{userAccountCtx.record}</Text>
-                                    </View>
-                                    <Pressable
-                                        style={({pressed}) => (pressed ? [styles.buttonPress, styles.button] : styles.button)}
-                                        onPress={openHallID}
-                                    >
-                                        <Entypo name="edit" size={18} color="black" />
-                                    </Pressable>
-                                </View>
-                            :
-
-                                <View></View>
-                        }
-
-                        <View style={styles.bookContainer}>
-                            <View style={styles.book}>
-                                <Text style={styles.bookLabel}>Number of bookings: </Text>
-                                <Text style={styles.bookText}>{numberOfReservations.length}</Text>
-                            </View>
-                        </View>
-                        <Pressable
-                            style={({pressed}) => (pressed ? [styles.logoutButtonPress, styles.logoutButton] : styles.logoutButton)}
-                            onPress={userAccountCtx.logout}
-                        >
-                            <View style={styles.logoutContainer}>
-                                <Text style={styles.logoutText}>Logout</Text>
-                            </View>
-                        </Pressable>
-                    </View>
-
+                <View style={styles.hallContainer}>
+                    <HallList Halls={bookedHalls} LocationOfUser={route.params.locationOfUser} booking={true} deleateReservations={deleateReservations}/>
                 </View>
             </View>
         );
     }
 }
 
-export default Account;
+export default Bookings;
 
 const styles = StyleSheet.create({
     container:{
         flex: 1,
         alignItems: 'center',
-        marginTop: 22
+
     },
-    subContainer1:{
+    hallContainer:{
         flex: 1,
-        // alignItems: 'center',
-        // marginTop: 6
-    },
-    imageContainer:{
-        width: 140,
-        height: 140,
-        borderRadius: 70,
-        overflow: 'hidden',
-        borderWidth: 2,
-    },
-    button:{
-        marginLeft: 60,
-        marginTop: 5,
-    },
-    buttonPress:{
-        opacity: 0.5,
-    },
-    image:{
-        width: '100%',
-        height: '100%',
-    },
-    subContainer2:{
-        flex: 3,
         alignItems: 'center',
-        marginTop: 25,
-        borderTopWidth: 1,
-        // marginTop: 6
-    },
-    informationContainer:{
-        flex: 1,
-        marginTop: 10,
-    },
-    nameContainer:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    name:{
-        flexDirection: 'row',
-        alignItems: 'center',
-        alignContent: 'center'
-    },
-    nameLabel:{
-        fontSize: 16,
-        fontWeight: 'bold'
-    },
-    nameText:{
-        fontSize: 16,
-    },
-    emailContainer:{
-        marginTop: 12,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        alignContent: 'center'
-    },
-    email:{
-        flexDirection: 'row',
-        alignItems: 'center',
-        alignContent: 'center',
-        borderTopWidth: 1,
-        paddingTop: 12,
-    },
-    emailLabel:{
-        fontSize: 16,
-        fontWeight: 'bold'
-    },
-    emailText:{
-        fontSize: 16,
-    },
-    bookContainer:{
-        marginTop: 12,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        alignContent: 'center'
-    },
-    book:{
-        flexDirection: 'row',
-        alignItems: 'center',
-        alignContent: 'center',
-        borderTopWidth: 1,
-        paddingTop: 12,
-    },
-    bookLabel:{
-        fontSize: 16,
-        fontWeight: 'bold'
-    },
-    bookText:{
-        marginLeft: 8,
-        fontSize: 16,
-    },
-    logoutContainer:{
-        backgroundColor: '#ba3c30',
-        alignContent: 'center',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 4,
-        shadowColor: 'black',
-        shadowOffset:{width: 0, height: 2},
-        shadowRadius: 6,
-        shadowOpacity: 0.25,
-        height: 40,
-        marginHorizontal: 45,
-        marginTop: 30
-    },
-    logoutText:{
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: 'white',
-    },
-    logoutButton:{
-        flex: 1,
-    },
-    logoutButtonPress:{
-        opacity: 0.7,
+        justifyContent:'center',
+        alignSelf:'stretch',
     },
 });
+  
