0d17a49bdb3b2afd04c8987e6b16016678c286e6
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2023-02-06 00:45:53 +0300

    Update auth.js

--- GradProject/util/auth.js
+++ GradProject/util/auth.js
@@ -6,6 +6,33 @@ import { collection, where, query, onSnapshot, addDoc, doc, updateDoc, getDocs }
 
 const API_KEY = 'AIzaSyDwVQ_iYQOWJcr_b4WUdLu1WpaeSg15uNg';
 
+async function sendVerification(mode, token) {
+  //console.log(mode, token);
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  if(token.rule >= 1){
+    var response = await axios.post(url, {
+      requestType: "VERIFY_EMAIL",
+      idToken: token.idToken,
+    });
+    //console.log(response);
+    return true;
+  }else{
+    return false;
+  }
+  
+}
+async function confirmVerification(mode, oobCode) {
+  //console.log(mode, oobCode);
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  var response ;
+
+  response = await axios.post(url, {
+    oobCode: oobCode,
+  });
+  
+  //console.log(response.data);
+  return response.data.emailVerified;
+}
 
 async function authenticate(mode, email, password, name, record, rule) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
@@ -15,7 +42,7 @@ async function authenticate(mode, email, password, name, record, rule) {
     returnSecureToken: true,
   });
 
-  console.log(response.data);
+  //console.log(response.data);
   const temp = response.data;
   var token ;
 
@@ -23,7 +50,8 @@ async function authenticate(mode, email, password, name, record, rule) {
     token = {...temp, 
       name: name,
       record: record,
-      rule: rule
+      rule: rule,
+      notificationsAddress: '',
     }
     //console.log(token);
     const UserID = await addDoc(collection(db, "Users"), {
@@ -32,7 +60,8 @@ async function authenticate(mode, email, password, name, record, rule) {
       uID: token.localId,
       Record: token.record,
       Rule: token.rule,
-      imageUrl: 'https://icons.veryicon.com/png/o/internet--web/55-common-web-icons/person-4.png'
+      imageUrl: 'https://icons.veryicon.com/png/o/internet--web/55-common-web-icons/person-4.png',
+      NotificationsAddress: ''
     }).
     then(()=>{
       token = {...token, 
@@ -50,14 +79,25 @@ async function authenticate(mode, email, password, name, record, rule) {
         uID: doc.data().uID,
         record: doc.data().Record,
         rule: doc.data().Rule,
-        image: doc.data().imageUrl
+        image: doc.data().imageUrl,
+        notificationsAddress: doc.data().NotificationsAddress,
       }
     });
+
+    // var isOwner = sendVerification('sendOobCode', token);
+    // if(isOwner){
+    //   token = {...token, 
+    //     rule: 0.8,
+    //   }
+    // }
+    //console.log(token);
+
   }
   //console.log(token);
 
   return token;
 }
+
 async function updateauthenticate(mode, idToken, email, password) {
   console.log(mode, idToken, email, password);
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
@@ -93,4 +133,8 @@ export function updateEmail(idToken, email) {
 }
 export function updatePassword(idToken, password) {
   return updateauthenticate('update', idToken, 'updatePassword', password);
+}
+
+export function confirmEmailVerification(oobCode) {
+  return confirmVerification('update', oobCode);
 }
