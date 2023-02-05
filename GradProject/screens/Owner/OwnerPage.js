c00ef3c37968dc9c9e0f813f8a2fa3c46d2813f0
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2023-02-06 00:51:12 +0300

    Update OwnerPage.js

--- GradProject/screens/Owner/OwnerPage.js
+++ GradProject/screens/Owner/OwnerPage.js
@@ -2,17 +2,45 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { Ionicons } from '@expo/vector-icons';
-import { Foundation } from '@expo/vector-icons'; 
+import { Ionicons, Foundation } from '@expo/vector-icons';
 
 import { GlobalStyles } from '../../constants/styles';
 
 import OwnerHome from './OwnerHome';
 import OwnerAccount from './OwnerAccount';
+import OwnerInbox from './OwnerInbox';
+import OwnerReservation from './OwnerReservation';
+import Chat from '../User/Chat';
 
+const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
-function OwnerPage(){
+function OwnerPage({locationOfUser}){
+
+    //Inbox page
+    function InboxOverview() {
+        return (
+            <Stack.Navigator
+            screenOptions={{
+                headerStyle: { backgroundColor: GlobalStyles.colors.primary10 },
+                headerTintColor: 'white',
+            }}
+            >
+            <Stack.Screen
+                name="Inbox"
+                component={OwnerInbox}
+                initialParams={{locationOfUser: locationOfUser}}
+            />
+            <Stack.Screen
+                name="Chat"
+                component={Chat}
+                initialParams={{locationOfUser: locationOfUser}}
+            />
+    
+            </Stack.Navigator>
+        );
+    }
+
     return(
         <>
             <NavigationContainer>
@@ -32,6 +60,29 @@ function OwnerPage(){
                             tabBarLabel: 'Home',
                             tabBarIcon: ({ color, size }) => (
                                 <Ionicons name="home" size={size} color={color} />
+                            ),
+                        }}
+                    />
+                    <BottomTabs.Screen
+                        name="InboxOverview"
+                        component={InboxOverview}
+                        options={{
+                            headerShown: false,
+                            title: 'Inbox',
+                            tabBarLabel: 'Inbox',
+                            tabBarIcon: ({ color, size }) => (
+                                <Ionicons name="mail" size={size} color={color} />
+                            ),
+                        }}
+                    />
+                    <BottomTabs.Screen
+                        name="Reservation"
+                        component={OwnerReservation}
+                        options={{
+                            title: 'Reservation',
+                            tabBarLabel: 'Reservation',
+                            tabBarIcon: ({ color, size }) => (
+                                <Foundation name="shopping-bag" size={size} color={color} />
                             ),
                         }}
                     />
