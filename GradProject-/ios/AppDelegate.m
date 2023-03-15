95a2dbb3b68249816ec2865c7bb6eee02932fa65
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2023-03-15 15:05:49 +0300

    Delete GradProject- directory

--- GradProject-/ios/AppDelegate.m
+++
@@ -1,6 +0,0 @@
-#import <GoogleMaps/GoogleMaps.h>  //ADD THIS
-@implementation AppDelegate
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    [GMSServices provideAPIKey:@"AIzaSyDwDENMUesiIxkFv_0EkVskbOe85fEvXm4"]; //ADD THIS
-}
